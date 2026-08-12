d4592a177d9671e2df7fbafccf639143925e7952
Belal Hossain <belalwebcreation@gmail.com>
2026-08-13 00:14:59 +0600

    Fix production SMTP SSL configuration

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,20 +2,20 @@ import nodemailer from "nodemailer";
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
   await transporter.verify();
 console.log("✅ SMTP Connected Successfully");
 
   const mailOptions = {
-    from: `"News Portal" <${process.env.EMAIL_USER}>`,
+    from: `"Royal Bangla News" <${process.env.EMAIL_USER}>`,
     to: options.email,
     subject: options.subject,
     html: options.message,
